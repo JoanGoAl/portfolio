5ab9e7ec4e29f6f70f9c97b1dd47187168702740
Joan Gonzalez <joangonzalezalbert@gmail.com>
2025-01-03 15:49:08 +0100

    fontawesome icon

--- src/pages/sections/presentacion.jsx
+++ src/pages/sections/presentacion.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import "./presentacion.css";
 import Avatar from "../../assets/avatar.webp";
 import { useTranslation } from "react-i18next";
+import { Email, Instagram, Linkedin } from "../../components/icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { faMailBulk } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 function Presentecion() {
     const { t, i18n } = useTranslation("translation", { keyPrefix: "presentacion" });
@@ -18,11 +23,11 @@ function Presentecion() {
                 <h1>Joan González.</h1>
                 <h2>{t("puesto")}</h2>
                 <div className="ilikeit">
-                    <p>Y me gusta</p>
+                    <p>{t("Me gusta")}</p>
                     <div className="caja__texto">
-                        <span>Trabajar en equipo</span>
-                        <span>Aprender cosas nuevas</span>
-                        <span>Resolver Problemas</span>
+                        <span>{t("mg.1")}</span>
+                        <span>{t("mg.2")}</span>
+                        <span>{t("mg.3")}</span>
                     </div>
                 </div>
                 <select onChange={(e) => onChange(e.target.value)} value={i18n.language}>
@@ -37,20 +42,21 @@ function Presentecion() {
                     </div>
                     <div className="info__contact">
                         <div>
+                            <FontAwesomeIcon icon={faEnvelope} color="var(--color-one)" />
                             <span>
                                 <a href="mailto:joangonzalezalbert@gmail.com">joangonzalezalbert@gmail.com</a>
                             </span>
                         </div>
                         <div>
+                            <FontAwesomeIcon icon={faWhatsapp} color="var(--color-one)" />
                             <span>
-                                <a href="mailto:joangonzalezalbert@gmail.com">joangonzalezalbert@gmail.com</a>
+                                <a href="tel:+34 684 411 728">684 411 728</a>
                             </span>
                         </div>
-                        <div>
-                            <span>
-                                <a href="mailto:joangonzalezalbert@gmail.com">joangonzalezalbert@gmail.com</a>
-                            </span>
-                        </div>
+                        {/* <div className="redes__sociales">
+                            <Instagram size={25} />
+                            <Linkedin size={25} />
+                        </div> */}
                     </div>
                 </div>
             </div>
