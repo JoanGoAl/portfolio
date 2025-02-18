8216a4a06fcf69efd2cd27b7813cd4611d0e62a3
Joan González <joangonzalezalbert@gmail.com>
2025-02-18 19:07:32 +0100

    favicon redimension y botones socialmeid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,35 @@ import TypeWriter from "@/components/typewriter/Typewriter";
 import styles from "./home.module.css"
 
 import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
+import { FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
+import { FaInstagram } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 
 export default function Home() {
   return (
     <section>
       <div className={styles.saludos_container}>
         <div className={styles.saludos}>
-          <h1>HI THERE</h1>
+          <h1>Hi there</h1>
           <h2>I am Joan González</h2>
           <div className={styles.destacar}>
             <TypeWriter strings={["Full Stack Developer", "Work as a team", "Learn new things"]} />
+          </div>
+          <div className={styles.social_media_container}>
+            <Link target="_blank" href={"https://www.linkedin.com/in/joan-gonz%C3%A1lez-albert-8b6892263/"}>
+              <FaLinkedin />
+            </Link>
+            <Link target="_blank" href={"https://wa.me/message/7BE43ZG3TFAVO1"}>
+              <FaWhatsapp />
+            </Link>
+            <Link target="_blank" href={"https://www.instagram.com/joan_gonzalez4?igsh=MWMwMDRra2FubTRjeQ%3D%3D&utm_source=qr"}>
+              <FaInstagram />
+            </Link>
+            <Link target="_blank" href={"https://github.com/JoanGoAl"}>
+              <FaGithub />
+            </Link>
           </div>
         </div>
         <div className={styles.container__photo__compare}>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,20 @@
+import { LinkProps } from "next/link";
+import React from "react";
+
 export interface PagesType {
     name: string,
     href: string,
-    icon?: React.ReactNode,
+    icon?: React.ReactElement,
     onClick?: () => void
+}
+
+export interface LinkButtonType extends LinkProps {
+
+    /**
+     * Define el color del botón.
+     * @default "primary"
+     */
+    variant?: "primary" | "icons",
+
+    children: React.ReactElement
 }
